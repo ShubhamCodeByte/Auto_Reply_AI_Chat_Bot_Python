ec297c9071fe905ad1704465cacb5c19a9d38486
ShubhamCodeByte <shubhamsingh22401@gmail.com>
2026-04-16 03:49:31 +0530

    the position coordinate testing toool

---
+++ tests/positionCheck.py
@@ -0,0 +1,9 @@
+import pyautogui as pui
+
+while True:
+  a = pui.position()
+  print(a)
+
+# 97 , 596  this is for the upper selection point
+# 641, 1002 this is for the lower selection point
+# 316, 837  this is the chat open click when the chat change or the new message will come
